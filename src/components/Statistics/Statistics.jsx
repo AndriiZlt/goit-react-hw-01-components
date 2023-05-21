5df17c838e2faf95f03f1276b5da1dd372f640e7
AndriiZlt <andreyzlot@gmail.com>
2023-05-21 18:53:53 +0300

    Title check implemented

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -3,17 +3,28 @@ import PropTypes from 'prop-types';
 import { Stat } from '../Stat/Stat';
 
 export const Statistics = ({ title, stats }) => {
-  return (
-    <section className={css.statistics}>
-      <h2 className={css.title}>{title}</h2>
-
-      <ul className={css.statList}>
-        {stats.map(({ id, label, percentage }) => (
-          <Stat key={id} label={label} percentage={percentage} />
-        ))}
-      </ul>
-    </section>
-  );
+  if (title) {
+    return (
+      <section className={css.statistics}>
+        <h2 className={css.title}>{title}</h2>
+        <ul className={css.statList}>
+          {stats.map(({ id, label, percentage }) => (
+            <Stat key={id} label={label} percentage={percentage} />
+          ))}
+        </ul>
+      </section>
+    );
+  } else {
+    return (
+      <section className={css.statistics}>
+        <ul className={css.statList}>
+          {stats.map(({ id, label, percentage }) => (
+            <Stat key={id} label={label} percentage={percentage} />
+          ))}
+        </ul>
+      </section>
+    );
+  }
 };
 
 Statistics.propTypes = {
