047a51e8b21abc98ea9b2313ba8ea3de33dd817a
AndriiZlt <andreyzlot@gmail.com>
2023-05-22 08:46:27 +0300

    Update Statistics.jsx

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -3,28 +3,16 @@ import PropTypes from 'prop-types';
 import { Stat } from '../Stat/Stat';
 
 export const Statistics = ({ title, stats }) => {
-  if (title) {
-    return (
-      <section className={css.statistics}>
-        <h2 className={css.title}>{title}</h2>
-        <ul className={css.statList}>
-          {stats.map(({ id, label, percentage }) => (
-            <Stat key={id} label={label} percentage={percentage} />
-          ))}
-        </ul>
-      </section>
-    );
-  } else {
-    return (
-      <section className={css.statistics}>
-        <ul className={css.statList}>
-          {stats.map(({ id, label, percentage }) => (
-            <Stat key={id} label={label} percentage={percentage} />
-          ))}
-        </ul>
-      </section>
-    );
-  }
+  return (
+    <section className={css.statistics}>
+      {title && <h2 className={css.title}>{title}</h2>}
+      <ul className={css.statList}>
+        {stats.map(({ id, label, percentage }) => (
+          <Stat key={id} label={label} percentage={percentage} />
+        ))}
+      </ul>
+    </section>
+  );
 };
 
 Statistics.propTypes = {
